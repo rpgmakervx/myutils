f6a80ec51994032fdf0752aac078d4c2210e6db1
rpgmakervx <583110127@qq.com>
2017-01-16 09:54:27 +0800

    修正请求头错误，host格式有问题导致400

--- myutils-db/src/test/java/org/easyarch/myutils/test/orm/parser/SQLParser.java
+++ myutils-db/src/test/java/org/easyarch/myutils/test/orm/parser/SQLParser.java
@@ -4,6 +4,7 @@ import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.BinaryExpression;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
+import net.sf.jsqlparser.expression.JdbcParameter;
 import net.sf.jsqlparser.expression.operators.relational.*;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.schema.Column;
@@ -15,12 +16,12 @@ import net.sf.jsqlparser.statement.select.SubSelect;
 import net.sf.jsqlparser.util.AddAliasesVisitor;
 import net.sf.jsqlparser.util.SelectUtils;
 import net.sf.jsqlparser.util.TablesNamesFinder;
+import org.easyarch.myutils.collection.CollectionUtils;
 import org.easyarch.myutils.test.orm.bean.SqlBean;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * Description :
@@ -34,7 +35,8 @@ public class SQLParser {
 
     @Test
     public void testParse() throws JSQLParserException {
-        Statement statement = CCJSqlParserUtil.parse("select a,b,c from test where test.id = ? and test.username = ? and  user.age = ? and user.create_at between ? and ? and label like ?");
+        Statement statement = CCJSqlParserUtil.parse("select a,b,c from test where test.id = ? and oid in (?,?,?) " +
+                "and  user.age = ? and user.create_at between ? and ? and label like ?");
         Select select = (Select) statement;
         PlainSelect plain = (PlainSelect) select.getSelectBody();
         Expression where = plain.getWhere();
@@ -43,22 +45,42 @@ public class SQLParser {
         System.out.println(columnNames);
     }
 
-    public void getWhereColumnNames(Expression whereAfter,List<SqlBean> columnNames){
-        Stack<Expression> stack = new Stack<>();
-        Expression current = whereAfter;
+//    public List<SqlBean> getWhereColumnNames(Expression whereAfter){
+//        if (whereAfter == null){
+//            return new ArrayList<>();
+//        }
+//        Stack<>
+//        while ()
+//        if (whereAfter instanceof BinaryExpression){
+//            BinaryExpression binaryExpression = (BinaryExpression) whereAfter;
+//            Expression leftExpression = binaryExpression.getLeftExpression();
+//            Expression rightExpression = binaryExpression.getRightExpression();
+//        }else if (whereAfter instanceof Between){
+//            Between between = (Between) whereAfter;
+//            //between 没有只有左子树有column，右子树没有
+//        }else if (whereAfter instanceof InExpression){
+//            InExpression inExpression = (InExpression) whereAfter;
+//        }else if (whereAfter instanceof LikeExpression){
+//            LikeExpression likeExpression = (LikeExpression) whereAfter;
+//        }
+//    }
 
-    }
 
     public void getWhereColumnName(Expression whereAfter,List<SqlBean> columnNames){
-
         if (whereAfter instanceof Column){
-            String columnName = ((Column) whereAfter).getColumnName();
             return;
         }
         if (whereAfter instanceof BinaryExpression){
             BinaryExpression binaryExpression = (BinaryExpression) whereAfter;
             Expression leftExpression = binaryExpression.getLeftExpression();
             Expression rightExpression = binaryExpression.getRightExpression();
+            if (leftExpression instanceof Column &&rightExpression instanceof JdbcParameter){
+                String columnName = ((Column) leftExpression).getColumnName();
+                SqlBean sqlBean = new SqlBean();
+                sqlBean.setColumnName(columnName);
+                sqlBean.setParams(1);
+                columnNames.add(sqlBean);
+            }
             // 访问左子树
             getWhereColumnName(leftExpression,columnNames);
             // 访问右子树
@@ -66,19 +88,41 @@ public class SQLParser {
         }else if (whereAfter instanceof Between){
             Between between = (Between) whereAfter;
             //between 没有只有左子树有column，右子树没有
+            Column column = (Column)between.getLeftExpression();
+            SqlBean sqlBean = new SqlBean();
+            sqlBean.setColumnName(column.getColumnName());
+            sqlBean.setParams(2);
+            columnNames.add(sqlBean);
             getWhereColumnName(between.getLeftExpression(),columnNames);
         }else if (whereAfter instanceof InExpression){
             InExpression inExpression = (InExpression) whereAfter;
+            Column column = (Column)inExpression.getLeftExpression();
+            ItemsList itemsList = inExpression.getRightItemsList();
+            if (itemsList instanceof ExpressionList){
+                ExpressionList expressionList = (ExpressionList) itemsList;
+                List<Expression> expressions = expressionList.getExpressions();
+                if (CollectionUtils.isNotEmpty(expressions)){
+                    SqlBean sqlBean = new SqlBean();
+                    sqlBean.setColumnName(column.getColumnName());
+                    sqlBean.setParams(expressions.size());
+                    columnNames.add(sqlBean);
+                }
+            }
             getWhereColumnName(inExpression.getLeftExpression(),columnNames);
         }else if (whereAfter instanceof LikeExpression){
             LikeExpression likeExpression = (LikeExpression) whereAfter;
+            Column column = (Column) likeExpression.getLeftExpression();
+            SqlBean sqlBean = new SqlBean();
+            sqlBean.setColumnName(column.getColumnName());
+            sqlBean.setParams(1);
+            columnNames.add(sqlBean);
             getWhereColumnName(likeExpression,columnNames);
         }
     }
 
     @Test
     public void testExisit() throws JSQLParserException {
-        Statement statement = CCJSqlParserUtil.parse("select a,b,c from test where exists ( select * from user) and id = ?");
+        Statement statement = CCJSqlParserUtil.parse("select a,b,c from test where id = ?");
         Select select = (Select) statement;
         PlainSelect plain = (PlainSelect) select.getSelectBody();
         Expression where = plain.getWhere();
@@ -92,39 +136,16 @@ public class SQLParser {
 
     @Test
     public void demo() throws JSQLParserException{
-//        CCJSqlParserManager pm = new CCJSqlParserManager();
-//
-//        StringBuffer stringBuffer = new StringBuffer();
-//        stringBuffer.append("update ac_operator op ");
-//        stringBuffer.append("set op.errcount=(");
-//        stringBuffer.append("(select case when op1.errcount is null then 0 else op1.errcount end as errcount ");
-//        stringBuffer.append("from ac_operator op1 ");
-//        stringBuffer.append("where op1.loginname = '中国' )+1");
-//        stringBuffer.append("),lastlogin='中国' ");
-//        stringBuffer.append("where PROCESS_ID=");
-//        stringBuffer.append("(select distinct g.id from tempTable g where g.ID='中国')");
-//        stringBuffer.append("and columnName2 = '890' and columnName3 = '678' and columnName4 = '456'");
-        Statement statement = CCJSqlParserUtil.parse("select a,b,c from test where exisits ( select * from user)");
-//        Statement statement = pm.parse(new StringReader(stringBuffer.toString()));
-
-        if (statement instanceof Select) {
-            //获得Update对象
-            Select select = (Select) statement;
-            PlainSelect plain = (PlainSelect) select.getSelectBody();
-            TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
-            List<String> tableList = tablesNamesFinder.getTableList(select);
-            //获得表名
-            System.out.println("table:"+tableList);
-            //获得where条件表达式
-            Expression where = plain.getWhere();
-            //初始化接收获得到的字段信息
-            StringBuffer allColumnNames = new StringBuffer();
-            //BinaryExpression包括了整个where条件，
-            //例如：AndExpression/LikeExpression/OldOracleJoinBinaryExpression
-            if(where instanceof BinaryExpression){
-                getWhereColumnName(where,allColumnNames);
-                System.out.println(allColumnNames.toString());
-            }
+        Statement statement = CCJSqlParserUtil.parse("select sex from test where age > 20 and id in (?,?,?)");
+        Select select = (Select) statement;
+        PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+        BinaryExpression binaryExpression = (BinaryExpression) plainSelect.getWhere();
+        InExpression inExpression = (InExpression) binaryExpression.getRightExpression();
+        ExpressionList expressionList = (ExpressionList) inExpression.getRightItemsList();
+        List<Expression> expressions = expressionList.getExpressions();
+        System.out.println(expressions);
+        for (Expression e:expressions){
+            System.out.println(e.getClass().getName());
         }
     }
 
