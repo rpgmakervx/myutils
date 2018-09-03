14e1dece57e5ddffd60f9c3aa982a907c78197a0
xingtianyu <583110127@qq.com>
2018-09-03 13:01:13 +0800

    leetcode刷题

--- myutils-algorithm/src/main/java/org/easyarch/myutils/leetcode/p9/Solution2.java
+++ myutils-algorithm/src/main/java/org/easyarch/myutils/leetcode/p9/Solution2.java
@@ -4,4 +4,26 @@ package org.easyarch.myutils.leetcode.p9;
  * Created by xingtianyu on 2018/9/3.
  */
 public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1000000001));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        int num = x;
+        int result = 0;
+        int remainder = 0;
+        while (num != 0){
+            remainder = num % 10;
+            num = num / 10;
+            if (num > Integer.MAX_VALUE / 10 || num == Integer.MAX_VALUE/10 && remainder == 7){
+                return false;
+            }
+            result = result * 10 + remainder;
+        }
+        return result == x;
+    }
 }
