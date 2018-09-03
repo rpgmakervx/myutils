777d7493d8d34b4b197389f0e5993f3724be3710
xingtianyu <xingtianyu@daojia-inc.com>
2018-09-03 13:55:55 +0800

    重试队列实现

--- myutils-algorithm/src/main/java/org/easyarch/myutils/leetcode/p13/Solution1.java
+++ myutils-algorithm/src/main/java/org/easyarch/myutils/leetcode/p13/Solution1.java
@@ -13,15 +13,15 @@ public class Solution1 {
     private static final Map<Character,Integer> KEYWORDS = new HashMap<>();
     static {
         KEYWORDS.put('I',1);
-        KEYWORDS.put('V',2);
-        KEYWORDS.put('X',3);
-        KEYWORDS.put('L',4);
-        KEYWORDS.put('C',5);
-        KEYWORDS.put('D',6);
-        KEYWORDS.put('M',7);
+        KEYWORDS.put('V',5);
+        KEYWORDS.put('X',10);
+        KEYWORDS.put('L',50);
+        KEYWORDS.put('C',100);
+        KEYWORDS.put('D',500);
+        KEYWORDS.put('M',1000);
     }
     public static void main(String[] args) {
-
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
     public static int romanToInt(String s) {
@@ -29,9 +29,14 @@ public class Solution1 {
             return -1;
         }
         char[] chars = s.toCharArray();
+        int front = 0;
+        int result = 0;
         for (int index=0;index<chars.length;index++){
-            KEYWORDS.get(chars[chars.length - 1]);
+            Integer num = KEYWORDS.get(chars[chars.length - 1 - index]);
+            //比上一个大做减法，否则做加法
+            result = (num < front)?result - num:result + num;
+            front = num;
         }
-        return 1;
+        return result;
     }
 }
